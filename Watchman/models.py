91d96ee1497306739a7c6f42ccf17218e1662544
Matthew Wollenweber <mjw@insomniac.tech>
2024-07-06 18:33:34 -0500

    wip

--- Watchman/models.py
+++ Watchman/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+
+class Client(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+
+class Domain(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    first_seen = models.DateTimeField(auto_now_add=True, blank=True)
+
+
+class ZoneList(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    update_interval = models.IntegerField(default=1440)
+    last_updated = models.DateTimeField(blank=True, null=True)
--- Watchman/settings.py
+++ Watchman/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     "bootstrap5",
     "django.contrib.admin",
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
