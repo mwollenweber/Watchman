37dffd828604727167ddc23e9675f6b8fe344684
Matthew Wollenweber <mjw@insomniac.tech>
2024-08-06 00:19:45 -0500

    WIP basic slack

---
+++ Watchman/management/commands/test_slack.py
@@ -0,0 +1,10 @@
+import logging
+from django.core.management.base import BaseCommand
+from Watchman.alerts.slackAlert import test
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        logger.info("meh")
+        test("This is my test message")
--- Watchman/slack/slack.py
+++
@@ -1,9 +0,0 @@
-import logging
-from slack_sdk import WebClient
-
-
-logger = logging.getLogger(__name__)
-
-def test():
-    client = WebClient()
-    api_response = client.api_test()
